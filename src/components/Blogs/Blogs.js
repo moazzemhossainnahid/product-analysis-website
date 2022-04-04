985e1c66a860ee1da8f345243024ea91d7d1a5df
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-04-04 19:02:37 +0600

    fixed blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -6,19 +6,20 @@ const Blogs = () => {
         <div className='container py-5'>
             <div className="txtDiv">
                 <h3 className="bg-danger py-3">Context API</h3>
-                <p className="">The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+                <p className="">The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.That’s one of the best concepts about React - you can create a bunch of components and have a fully maintainable and concise application, without having to create a super huge component to deal with your whole application.</p>
             </div>
             <div className="txtDiv">
                 <h3 className="bg-warning py-3">Semantic Tags</h3>
-                <p className="">Semantic HTML tags provide information about the contents of those tags that goes beyond just how they look on a page. Text that is enclosed in the tag is immediately recognized by the browser as some type of coding Language.
-                Examples of semantic HTML tags.
+                <p className="">Semantic HTML tags provide information about the contents of those tags that goes beyond just how they look on a page. Text that is enclosed in the tag is immediately recognized by the browser as some type of coding Language.The semantic HTML tags help the search engines and other user devices to determine the importance and context of web pages.
 
             <h4 className="pt-4">List of Semantic tags</h4>
             
-                    <h6>h1 to h6</h6>
-                    <h6>blockquote</h6>
-                    <h6>code</h6>
-                    <h6>em</h6>
+                    <h6>header</h6>
+                    <h6>nav</h6>
+                    <h6>aside</h6>
+                    <h6>article</h6>
+                    <h6>section</h6>
+                    <h6>footer</h6>
 
                 </p>
             </div>
